b2e44af68b37f2c2ecacc94d7d7eea64c94445a6
AdityaMaheshwari30114 <adityamaheshwari30114@gmail.com>
2024-09-21 19:49:00 +0530

    add routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterModule } from '@angular/router'; 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
